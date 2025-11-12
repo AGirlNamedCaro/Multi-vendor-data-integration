4a561d2aa947660df5397109dce42b2c3a9dcece
Carolina Contreras Issa <AGirlNamedCaro@gmail.com>
2025-11-11 22:28:46 -0700

    added support for csv

--- credit_report.rb
+++ credit_report.rb
@@ -1,8 +1,9 @@
 require "json"
 require "nokogiri"
+require "csv"
 
 class CreditReport
-  APPROVED_VENDORS = ["creditscore360", "bizcreditplus"].freeze
+  APPROVED_VENDORS = ["creditscore360", "bizcreditplus", "enterprisecreditdata"].freeze
 
   def initialize(data, vendor)
     @data = data
@@ -25,15 +26,27 @@ class CreditReport
       credit_score_360_adapter(parsed_data)
     elsif @vendor == "bizcreditplus"
       bizcreditplus_adapter(parsed_data)
+    else
+      enterprisecreditdata_adapter(parsed_data)
     end
   end
 
   def parse_data
-    JSON.parse(@data)
-  rescue JSON::ParserError
-    Nokogiri::XML(@data)
-  rescue Nokogiri::XML::SyntaxError
-    nil
+    begin
+      return JSON.parse(@data)
+    rescue JSON::ParserError
+      xml_doc = Nokogiri::XML(@data)
+      if xml_doc.root.nil? || (xml_doc.errors.any? && xml_doc.root.name == "parsererror")
+        begin
+          csv = CSV.parse(@data, headers: true)
+          return csv
+        rescue CSV::MalformedCSVError
+          raise "Unsupported data format"
+        end
+      else
+        return xml_doc
+      end
+    end
   end
 
   def credit_score_360_adapter(parsed_data)
@@ -58,6 +71,20 @@ class CreditReport
       business_credit_score: parsed_data.xpath("//credit_data/business_score").text.to_i,
       personal_credit_score: parsed_data.xpath("//credit_data/personal_score").text.to_i,
       report_date: parsed_data.xpath("//credit_data/report_date").text,
+      data_source: @vendor,
+    }
+  end
+
+  def enterprisecreditdata_adapter(parsed_data)
+    row = parsed_data.first.to_h
+    {
+      business_name: row["business_name"],
+      tax_id: row["tax_id"],
+      owner_name: row["owner_name"],
+      owner_ssn: row["owner_ssn"],
+      business_credit_score: row["business_score"].to_i,
+      personal_credit_score: row["personal_score"].to_i,
+      report_date: row["report_date"],
       data_source: @vendor,
     }
   end
--- test/credit_report_test.rb
+++ test/credit_report_test.rb
@@ -29,4 +29,17 @@ class CreditReportTest < Minitest::Test
     assert_equal "2025-01-15", normalized_report[:report_date]
     assert_equal "bizcreditplus", normalized_report[:data_source]
   end
+
+  def test_parses_complete_valid_csv
+    data = File.read("test/fixtures/credit_report.csv")
+    normalized_report = CreditReport.new(data, "enterprisecreditdata").get_report
+    assert_equal "Joe's Pizza LLC", normalized_report[:business_name]
+    assert_equal "12-3456789", normalized_report[:tax_id]
+    assert_equal "Joseph Smith", normalized_report[:owner_name]
+    assert_equal "123-45-6789", normalized_report[:owner_ssn]
+    assert_equal 720, normalized_report[:business_credit_score]
+    assert_equal 680, normalized_report[:personal_credit_score]
+    assert_equal "2025-01-15", normalized_report[:report_date]
+    assert_equal "enterprisecreditdata", normalized_report[:data_source]
+  end
 end
